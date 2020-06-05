3047405e2eda20758391c9ce74fb68046c12af75
OlegZhevnyak <ffieryice@gmail.com>
2020-06-05 18:52:37 +0300

    NewUser Steps of TrashMail added.

---
+++ src/test/java/steps/TrashMailSteps.java
@@ -0,0 +1,36 @@
+package steps;
+
+import pages.trash_mail.MainPage;
+
+public class TrashMailSteps {
+
+    public static void newUserTabClick() {
+        MainPage.webElementNewUserTab().click();
+    }
+
+    public static void usernameType(String username) {
+        MainPage.webElementUsernameTextField().sendKeys(username);
+    }
+
+    public static void passwordType(String password) {
+        MainPage.webElementPasswordTextField().sendKeys(password);
+    }
+
+    public static void confirmPasswordType(String confirmPassword) {
+        MainPage.webElementPasswordConfirmTextField().sendKeys(confirmPassword);
+    }
+
+    public static void realEmailType(String realEmail) {
+        MainPage.webElementRealEmailTextField().sendKeys(realEmail);
+    }
+
+    public static void registerButtonClick() {
+        MainPage.webElementRegisterButton().click();
+    }
+
+    public void printResult(){
+        String resultAlertString = MainPage.ResultAlert().getText();
+        System.out.println(resultAlertString);
+    }
+
+}
